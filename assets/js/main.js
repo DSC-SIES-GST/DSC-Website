c1c9a04f1dbbf9110145ed50736cf9d4e6c3e994
Shivam Gupta <shivamrvgupta@gmail.com>
2023-04-10 16:39:37 +0530

    final done

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+
+//for our team section
+
+const btns = document.querySelectorAll('.btn');
+const storeProducts = document.querySelectorAll('.store-product');
+
+for (i = 0; i < btns.length; i++) {
+
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const filter = e.target.dataset.filter;
+
+
+        storeProducts.forEach((product) => {
+            if (product.classList.contains(filter)) {
+                product.style.display = 'flex'
+            } else {
+                product.style.display = 'none'
+            }
+
+        });
+    });
+};
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
